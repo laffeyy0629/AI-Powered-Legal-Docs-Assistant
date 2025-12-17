fbccf28fdaf21cfb09796996f23e47e117294904
Ace-zzzz <escobia357@gmail.com>
2025-12-17 23:42:07 +0800

    feat: User Model & ignore applciation.properties

---
+++ ai-docs-assistant/src/main/java/com/isaqcasey/aidocsassistant/Model/User.java
@@ -0,0 +1,39 @@
+package com.isaqcasey.aidocsassistant.Model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long iD;
+
+    @JsonProperty("user_name")
+    @Column(unique = true)
+    private String userName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @JsonProperty("o_auth_provider")
+    private String oAuthProvider;
+
+    @JsonProperty("o_auth_id")
+    private String oAuthId;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
